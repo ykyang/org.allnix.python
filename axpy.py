e072d531c88b607eee1a760d8ce6de4687134538
Yi-Kun Yang <ykyang@gmail.com>
2017-08-22 07:38:33 -0500

    Refine axpy timing

--- axpy.py
+++ axpy.py
@@ -5,25 +5,27 @@ import numpy as np
 
 def loop():
     n = 1000000
-    count = 1000
+    count = 10
     a = 0.5
 
     x = array('d', range(n))
     y = array('d', range(n))
 
     for i in range(n):
-        x[i] = 1
+        x[i] = i
         y[i] = 1.0
-
+    start = time.time()
     for j in range(count):
         for i in range(n):
             y[i] = a * x[i] + y[i]
-
+    end = time.time()
     print(y[n - 1])
+    print(end - start)
 
 
-start = time.time()
+
+
+
 loop()
-end = time.time()
-print(end - start)
+
 
