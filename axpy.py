69460f6c200d2c10a7ed88a1cb401df1d6daea91
Yi-Kun Yang <ykyang@gmail.com>
2017-08-23 13:42:14 -0500

    Intermediate: Test more complicated expression

--- axpy.py
+++ axpy.py
@@ -65,18 +65,78 @@ def numpyloop():
     print(y[n - 1])
     print(end - start)
 
+def myfunc(x, y):
+    if x % 2 == 0:
+        y = x
+
+def cond():
+    n = 100
+    count = 10000000
+    x = np.arange(n, dtype=np.float64)
+#    z = np.arange(n, dtype=np.float64)
+    y = np.zeros(n, dtype=np.float64)
+    z = np.zeros(n, dtype=np.float64)
+    #print(x)
+    
+    for i in range(n):
+        x[i] = i
+        y[i] = 0.0
+        z[i] = 1.5 * (i+2)
+#    vfunc = np.vectorize(myfunc)
+
+#    d = 1./13.
+    start = time.time()
+    for j in xrange(count):
+#        y = x/13. + z - 10
+        y = x/13.
+        y += z
+        y -= 10
+#        condlist = [z > 15., z <= 15.]
+#        choicelist = [x/13. + z -10., x/15. + z]
+#        y = np.select(condlist, choicelist)
+
+#        vfunc(x,y)
+        
+#    for d in np.nditer(x):
+#        print(d)
+#    it = np.nditer([x,y], op_flags=['readwrite'])
+#    for xx,yy in it:
+#        yy[...] = xx if xx%2 == 0 else yy
+    
+#    y = it.operands[1]
+
+#    for j in xrange(count):
+#        condlist = [x%2 == 0]
+#        choicelist = [x]
+#        np.select(condlist, choicelist)
+#        y = x[x % 2 == 0]
+    end = time.time()
+#    print(x)    
+#    print(z)
+#    print(x[x<5])
+#    print(z[x<5])
+#    x[x<5] = z[x<5]
+#    print(x)
+#    print(z)
+    #print(y)
+    print(y[n - 1])
+    print(end - start)
+
+
+cond()
+
 # Numpy axpy from Luke
 # count = 1000 -> 2.72672486305
-numpyloop()
+#numpyloop()
 
 # Python array:
 # count = 10 -> 1.9 sec
 # count = 1000 -> 184.210150957 sec
-loop()
+#loop()
 # Numpy array:
 # What the *$%@, Numpy is slower?
 # count = 10 -> 2.9 sec
 # count = 1000 -> 291.18137908
-numloop()
+#numloop()
 
 
