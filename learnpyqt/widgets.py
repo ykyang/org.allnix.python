65130941ab403a2cfc4d1b3bc6c7230b47529132
Yi-Kun Yang <ykyang@gmail.com>
2020-08-03 21:12:23 -0500

    LearnPyQt: Page 37

--- learnpyqt/widgets.py
+++ learnpyqt/widgets.py
@@ -3,4 +3,27 @@ import sys
 import PySide2.QtCore as qtc
 import PySide2.QtWidgets as qtw
 
-class MainWindow(qtw.QMainWindow)
+class MainWindow(qtw.QMainWindow):
+    def __init__(self):
+        super().__init__()
+        
+        self.setWindowTitle('My App')
+        
+        widget = qtw.QLabel('Hello')
+        font = widget.font()
+        font.setPointSize(30)
+        widget.setFont(font)
+        widget.setAlignment(qtc.Qt.AlignHCenter | qtc.Qt.AlignVCenter)
+        
+        self.setCentralWidget(widget)
+        
+app = qtw.QApplication(sys.argv)
+
+window = MainWindow()
+window.show()
+
+app.exec_()
+   
+        
+        
+        
