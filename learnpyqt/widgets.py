036e1a39e4e364686766b627b83aaebd582c2b21
Yi-Kun Yang <ykyang@gmail.com>
2020-08-05 20:09:42 -0500

    LearnPyQt: Page 50

--- learnpyqt/widgets.py
+++ learnpyqt/widgets.py
@@ -1,6 +1,7 @@
 import sys
 
 import PySide2.QtCore as qtc
+import PySide2.QtGui as qtg
 import PySide2.QtWidgets as qtw
 
 class MainWindow(qtw.QMainWindow):
@@ -9,13 +10,53 @@ class MainWindow(qtw.QMainWindow):
         
         self.setWindowTitle('My App')
         
-        widget = qtw.QLabel('Hello')
-        font = widget.font()
-        font.setPointSize(30)
-        widget.setFont(font)
-        widget.setAlignment(qtc.Qt.AlignHCenter | qtc.Qt.AlignVCenter)
+#         widget = qtw.QLabel('Hello')
+#         font = widget.font()
+#         font.setPointSize(30)
+#         widget.setFont(font)
+#         widget.setAlignment(qtc.Qt.AlignHCenter | qtc.Qt.AlignVCenter)
+        
+#         widget.setPixmap(qtg.QPixmap('source/basic/otje.jpg'))
+#         widget.setScaledContents(True)
+        
+#         widget = qtw.QCheckBox('This is a checkbox')
+#         widget.setCheckState(qtc.Qt.Checked)
+#         
+#         # For tristate: widget.setCheckState(Qt.PartiallyChecked)         
+#         #widget.setTriState(True)
+#         widget.setTristate(True)
+#         widget.stateChanged.connect(self.show_state)
+
+#         widget = qtw.QComboBox()
+#         widget.addItems(['One', 'Two', 'Three'])
+#         widget.setEditable(True)
+#         
+#         widget.currentIndexChanged.connect(self.index_changed)
+#         #print(type(widget.currentIndexChanged))
+#         widget.currentTextChanged.connect(self.text_changed)                 
+        
+        widget = qtw.QListWidget()
+        widget.addItems(['One', 'Two', 'Three'])
+        widget.currentItemChanged.connect(self.item_changed)
+        widget.currentTextChanged.connect(self.text_changed)
+        
         
         self.setCentralWidget(widget)
+
+    def item_changed(self, item):
+        print(item.text())
+        
+    def index_changed(self, i):
+        print(i)
+        
+        
+    def text_changed(self, s):
+        print(s)
+        
+    def show_state(self, s):
+        print(s == qtc.Qt.Checked)
+        print(s)
+        
         
 app = qtw.QApplication(sys.argv)
 
