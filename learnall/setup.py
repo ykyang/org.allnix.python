ccb2b8071e350a40a9bc3e4cc1e1c6c11b59ff48
Yi-Kun Yang <ykyang@gmail.com>
2021-07-18 14:27:54 -0500

    Add minimal setup.py

--- learnall/setup.py
+++ learnall/setup.py
@@ -6,13 +6,6 @@ setup(
     author='Yi-Kun Yang',
     author_email='ykyang@gmail.com.com',
     packages=['learnall'], #, 'package_name.test'],
-    #scripts=['bin/script1','bin/script2'],
-    #url='http://pypi.python.org/pypi/PackageName/',
-    #license='LICENSE.txt',
-    description='An awesome package that does something',
-    #long_description=open('README.txt').read(),
-    #    install_requires=[
-    #        "Django >= 1.1.1",
-    #        "pytest",
-    #    ],
+    license='LICENSE',
+    description='A package to learn Pythong',
 )
