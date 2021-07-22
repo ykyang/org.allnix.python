e5daf4aa5bbd6c1f88fd8744ad5239317db5a96d
Yi-Kun Yang <ykyang@gmail.com>
2021-07-22 07:40:50 -0500

    Clean flask

--- learnall/learn_flask.py
+++ learnall/learn_flask.py
@@ -1,6 +1,5 @@
-from flask import Flask
+#from flask import Flask
 from learnall import app
-import learnall.vtkservice
 
 #app = Flask(__name__)
 
@@ -8,8 +7,6 @@ import learnall.vtkservice
 @app.route('/hello')
 def hello():
     return "Hello World!"
-
-print(learnall.vtkservice.hellovtk())
 
 if __name__ == '__main__':
     app.run()
--- learnall/learnall/__init__.py
+++ learnall/learnall/__init__.py
@@ -3,7 +3,7 @@ from flask import Flask
 
 app = Flask(__name__)
 
-import learnall
+import learnall.vtkservice
 
 def hello():
     import wx
